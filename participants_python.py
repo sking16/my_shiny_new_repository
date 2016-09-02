dc87bca8e256b48359828e39e02b5aedfb4df6fc
Scott King <sking@linz.govt.nz>
2016-09-02 13:25:19 +1200

    Please enter the commit message for your changes. Lines starting

--- participants_python.py
+++ participants_python.py
@@ -1,3 +1,4 @@
 #I would like to write some Python code here that adds two numbers but I don't know how to do that
 #I would like to write some code that prints out the name of each participant of this workshop
+print("Scott King")
 #Can you help me using a Pull Requests?
