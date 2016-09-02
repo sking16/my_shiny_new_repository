b0e0a8216e2bf05609b6db44d0ce68d796469eb2
Aleksandra Pawlik <aleksandra.n.pawlik@gmail.com>
2016-09-02 13:12:52 +1200

    Python file

---
+++ participants_python.py
@@ -0,0 +1,3 @@
+#I would like to write some Python code here that adds two numbers but I don't know how to do that
+#I would like to write some code that prints out the name of each participant of this workshop
+#Can you help me using a Pull Requests?
